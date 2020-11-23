cb3402920526b18f26fa152f8bb6bc3c26504045
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-23 14:09:31 +0000

    added employee class details and tests for setter/getter name

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,13 @@
+package Staff;
+
+public class Employee {
+    private String name;
+    private String NInumber;
+    private double salary;
+
+    public Employee(String name, String NInumber, double salary){
+        this.name = name;
+        this.NInumber = NInumber;
+        this.salary = salary;
+    }
+}
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,2 +1,24 @@
+import Staff.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class EmployeeTest {
+    private Employee employee;
+
+    @Before
+    public void before(){
+        employee = new Employee("Reliable Bob", "HG986556K", 40000);
+    }
+    @Test
+    public void employeeHasName(){
+        assertEquals("Reliable Bob", employee.getName());
+    }
+    @Test
+    public void canSetEmployeeName(){
+        employee.setName("Lazy Bob");
+        assertEquals("Lazy Bob", employee.getName());
+    }
 }
